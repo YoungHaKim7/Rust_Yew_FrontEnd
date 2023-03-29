b4a0a73ef54ea0dbbc8f012a950a1bb011ad2d74
YoungHa Kim <ytok1108@aol.com>
2023-03-29 20:57:24 +0900

    yew Rust tutorial__mod_ Devide & Conquer 02

---
+++ tutorial/yew_tu04_mod02/src/app/videosdetails.rs
@@ -0,0 +1,19 @@
+use yew::function_component;
+use yew::prelude::*;
+
+use super::structvideo::Video;
+
+#[derive(Clone, Properties, PartialEq)]
+pub struct VideosDetailsProps {
+    pub video: Video,
+}
+
+#[function_component(VideoDetails)]
+pub fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{ video.title.clone() }</h3>
+            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+        </div>
+    }
+}
